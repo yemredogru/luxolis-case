148cd2d7c9d64f7407af9993bbbbeba20a4b9a0b
Yasar Emre Dogru <71075406+yemredogru@users.noreply.github.com>
2023-10-22 13:37:28 +0300

    success page created

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,11 @@
+import './Home.css'
+
+export default function HomePage(){
+    return(
+        <div className="home-container">
+            <li>
+              <a href={`/`}>Return to the Login Page</a>
+            </li>
+        </div>
+    )
+}
